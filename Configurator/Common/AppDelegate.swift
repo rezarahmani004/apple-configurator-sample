7ce57611814153e52b5bf7ef91c0651c63d549d7
Reza Rahmani <rezarahmani004@gmail.com>
2026-03-19 15:21:59 -0400

    Fix: update AppDelegate and connection logic

--- Configurator/Common/AppDelegate.swift
+++ Configurator/Common/AppDelegate.swift
@@ -7,6 +7,7 @@
 // disclosure or distribution of this material and related documentation
 // without an express license agreement from NVIDIA CORPORATION or
 // its affiliates is strictly prohibited.
+// test change
 
 import Foundation
 import SwiftUI
